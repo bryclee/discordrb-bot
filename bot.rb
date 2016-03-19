6a6fe74f74a88ec4d6df7133a9cfc5933b2d7de5
Bryan Lee <pygmize.me@gmail.com>
2016-03-19 04:32:14 +0000

    (refactor) Add enableStore method to move store behavior out of main file

--- bot.rb
+++ bot.rb
@@ -20,41 +20,9 @@ bot.message(start_with: /meow/i) {|event| event.respond MiscCommands.meow()}
 bot.mention(content: /<@.*> servers/) {|event| event.respond StatusCommands.get_servers(bot)}
 bot.mention(content: /<@.*> channels/) {|event| event.respond StatusCommands.get_channels(bot)}
 
-# Testing writing to file, reading from file
-STORE_REGEX = /blee store (.*)/
-bot.message(with_text: STORE_REGEX) do |event|
-	message = STORE_REGEX.match(event.message.content)[1]
-	with_error_handling(event) do
-		StoreCommands.write(message)
-		event.respond "Stored: " + message
-	end
-end
+# Turn on store commands
+StoreCommands.enableStore(bot)
 
-bot.message(with_text: /blee read store/) do |event|
-	with_error_handling(event) do
-		data = StoreCommands.read()
-		event.respond "Store contents:"
-		event.respond data
-	end
-end
-
-bot.message(with_text: /blee clear store/) do |event|
-	with_error_handling(event) do
-		StoreCommands.clear()
-		event.respond "Cleard store"
-	end
-end
-
-# Accepts a block, runs it and if an error occurs then returns the error
-def with_error_handling(event)
-	begin
-		yield
-	rescue => e
-		event.respond "Had an error: " + e.class.to_s
-		raise e
-	end
-end
-	
 
 # Run the bot
 bot.run
--- lib/store.rb
+++ lib/store.rb
@@ -1,8 +1,5 @@
 module StoreCommands
     @filename = 'list.txt'
-    def initialize(name)
-        @filename = name
-    end
     
     module_function
     
@@ -31,6 +28,49 @@ module StoreCommands
     end
     
     def clear()
-        File.delete(@filename)
+        open(@filename, 'w')
+        return nil
     end
+    
+    # Enable store functions for bot
+    def enableStore(bot)
+        # Testing writing to file, reading from file
+        store_regex = /blee write store (.*)/
+        bot.message(with_text: store_regex) do |event|
+        	message = store_regex.match(event.message.content)[1]
+        	with_error_handling(event) do
+		        StoreCommands.write(message)
+        		event.respond "Stored: " + message
+    	    end
+        end
+
+        bot.message(with_text: /blee read store/) do |event|
+        	with_error_handling(event) do
+        		data = StoreCommands.read()
+        		if data.nil? || data.empty?
+		        	event.respond "No data in store"
+		        else
+			        event.respond "Store contents:"
+			        event.respond data
+        		end
+	        end
+        end
+
+        bot.message(with_text: /blee clear store/) do |event|
+	        with_error_handling(event) do
+		        StoreCommands.clear()
+        		event.respond "Cleared store"
+        	end
+        end 
+    end
+end
+
+# Accepts a block, runs it and if an error occurs then returns the error
+def with_error_handling(event)
+	begin
+		yield
+	rescue => e
+		event.respond "Had an error: " + e.class.to_s
+		raise e
+	end
 end
