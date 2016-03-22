15cc209289812871be822c7bddad8351594874be
Bryan Lee <pygmize.me@gmail.com>
2016-03-22 09:16:51 +0000

    (html_parser) Add regex for first half of element

--- lib/bot/html_parser.rb
+++ lib/bot/html_parser.rb
@@ -2,4 +2,12 @@ class HTMLParser
     def initialize(html)
         @html = html
     end
+    
+    # Find element of type
+    def find_element(type)
+        regex = Regexp.new("<#{type}[\s>]")
+        
+        elements = @html.to_enum(:scan, regex).map {|m| [m, Regexp.last_match.offset(0)[0]]}
+        puts elements
+    end
 end
--- tests/test_html_parser.rb
+++ tests/test_html_parser.rb
@@ -3,20 +3,21 @@ require 'bot/html_parser'
 
 class HTMLParserSpec < Test::Unit::TestCase
     
-    def test_selector()
+    def test_selector_simple_case()
         html = %{
             <body>
               <div>
                 <span>Hello</span>
+                <span class="something">Complex</span>
               </div>
             </body>
         }
         
         parser = HTMLParser.new(html)
         
-        span = parser.findElement(:span)
+        span = parser.find_element(:span)
         
-        assert_equal span.element, "<span>Hello</span>"
+        assert_equal span.html, "<span>Hello</span>"
     end
     
 end
